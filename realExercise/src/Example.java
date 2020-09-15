5f85fdd2cfcd0acc8cdd306f9b3745d2acbb0545
Manan Charania <36240714+1-n-only-nobody@users.noreply.github.com>
2020-09-15 17:00:13 +0530

    Day 1 Exercise 1

---
+++ realExercise/src/Example.java
@@ -0,0 +1,10 @@
+package com.training;
+
+public class Example{
+
+    private double rupee = 0;
+    public double getRupee(double dollar){
+        rupee = (dollar * 73.6);
+        return rupee;
+    }
+}
---
+++ realExercise/src/Test.java
@@ -0,0 +1,12 @@
+package com.training;
+
+
+public class Test{
+
+    public static void main(String[] args) {
+        
+        Example ex1 = new Example();
+
+        System.out.println(ex1.getRupee(500));
+    }
+}
